319c992c6d0fd117fa068a5c20818194f637adc4
Muhammad Mustapha Miah <muhammad_mustapha_miah@live.co.uk>
2020-05-18 15:44:40 +0100

    Implemented server and browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -11,5 +11,36 @@ gulp.task("watch", function () {
   gulp.watch("scss/**/*.scss", gulp.series("sass"));
 });
 
-
-
+const nodemon = require("gulp-nodemon");
+const browserSync = require("browser-sync").create();
+const config = require("./package").gulp;
+const serve = () => {
+  let started = false;
+  browserSync.init(null, {
+    proxy: "http://localhost:4000",
+    files: [
+      `${config.selectors.html}`,
+      `${config.src.css}${config.selectors.css}`,
+      `${config.src.js}${config.selectors.js}`,
+    ],
+    browser: "google chrome",
+    port: 7000,
+    reloadDelay: 1000,
+  });
+  return nodemon({
+    script: config.main.server,
+    ignore: [
+      `${config.src.js}${config.selectors.js}`,
+      `${config.src.scss}${config.selectors.scss}`,
+    ],
+    env: { NODE_ENV: "development" },
+  }).on("start", () => {
+    if (!started) {
+      browserSync.reload();
+    } else {
+      started = false;
+    }
+  });
+};
+gulp.task("serve", serve);
+// module.exports = serve;
